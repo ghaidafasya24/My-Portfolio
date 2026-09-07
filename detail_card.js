34c662f6749acdb8998c1278597684c8f7c5c2f4
ghaidafasya24 <ghaidafasya5@gmail.com>
2026-09-07 23:37:41 +0700

    fixed cards details

---
+++ detail_card.js
@@ -0,0 +1,11 @@
+function openProjectModal() {
+        document.getElementById("projectModal").classList.remove("hidden");
+
+        document.getElementById("projectModal").classList.add("flex");
+      }
+
+      function closeProjectModal() {
+        document.getElementById("projectModal").classList.remove("flex");
+
+        document.getElementById("projectModal").classList.add("hidden");
+      }
